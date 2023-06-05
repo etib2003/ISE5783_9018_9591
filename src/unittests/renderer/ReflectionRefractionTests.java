ff7801b790ebec49a5038a5f2c8d435963382b9f
etib2003 <etib2003@gmail.com>
2023-06-05 21:27:46 +0300

    .

--- src/unittests/renderer/ReflectionRefractionTests.java
+++ src/unittests/renderer/ReflectionRefractionTests.java
@@ -5,9 +5,11 @@ import static java.awt.Color.*;
 import org.junit.jupiter.api.Test;
 
 import geometries.Plane;
+import geometries.Polygon;
 import geometries.Sphere;
 import geometries.Triangle;
 import lighting.AmbientLight;
+import lighting.PointLight;
 import lighting.SpotLight;
 import primitives.*;
 import renderer.*;
@@ -104,96 +106,53 @@ public class ReflectionRefractionTests {
 				.writeToImage();
 	}
 
+
+	/**
+	 * Produce a picture of a pyramid lighted by a spot light with a mirror effects
+	 */
 	@Test
-	public void effectTests() {
-		Scene scene = new Scene("Custom2 Image Test");
+	public void pyramidTransparentSphere() {
+		Camera camera = new Camera(new Point(-140, 20, 35), new Vector(1, -0.15, -0.25), new Vector(1, 0, 4))
+				.setVPSize(200, 200).setVPDistance(1000);
 
-		Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)).setVPSize(200, 200)
-				.setVPDistance(1000);
+		scene.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
 
-		scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.15));
-
-		// Define the triangle
-		Triangle triangle = (Triangle) new Triangle(new Point(-75, -75, -150), new Point(0, 75, -150),
-				new Point(75, -75, -150)).setEmission(new Color(255, 255, 0))
-				.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60));
-
-		// Define the spheres
-		Sphere sphere1 = (Sphere) new Sphere(new Point(-100, -100, -150), 50).setEmission(new Color(255, 0, 0))
-				.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60));
-		Sphere sphere2 = (Sphere) new Sphere(new Point(100, -100, -150), 50).setEmission(new Color(0, 255, 0))
-				.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60));
-		Sphere sphere3 = (Sphere) new Sphere(new Point(0, 100, -150), 50).setEmission(new Color(0, 0, 255))
-				.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60));
-
-		Plane plane1 = (Plane) new Plane(new Point(-75, -75, -150), new Point(0, 75, -150), new Point(100, -100, -150))
-				.setEmission(new Color(0, 0, 0)).setMaterial(new Material().setkR(1));
-
-		scene.geometries.add(/*triangle,*/ sphere1, sphere2, /*sphere3,*/ plane1);
-
-		
-		
 		scene.geometries.add( //
-				new Sphere(new Point(0, 0, -50), 80d).setEmission(new Color(255, 50, 0)) //
-						.setMaterial(new Material().setKd(0.4).setKs(0.3).setShininess(100).setkT(0.3)),
-				new Sphere(new Point(0, 0, -50), 40d).setEmission(new Color(0, 255, 25)) //
-						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100)));
-		
-		
-		// Define the light source
-		//scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(60, 50, 0), new Vector(0, 0, -1)).setkL(4E-5).setkQ(2E-7));
+				new Plane(new Point(10, 0, -30), new Point(0, 10, -30), new Point(-10, 0, -30))
+						.setEmission(new Color(160, 0, 160))
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60).setkT(0).setkR(1)),
+				new Polygon(new Point(10, 0, -10), new Point(0, 10, -10), new Point(-10, 0, -10),
+						new Point(0, -10, -10))
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60).setkT(0.2).setkR(0)),
+				new Polygon(new Point(10, 0, -10), new Point(0, 10, -10), new Point(0, 10, 0), new Point(10, 0, 0))
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60).setkT(0.4)),
+				new Polygon(new Point(10, 0, -10), new Point(0, -10, -10), new Point(0, -10, 0), new Point(10, 0, 0))
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60).setkT(0).setkR(1)),
+				new Polygon(new Point(-10, 0, -10), new Point(0, 10, -10), new Point(0, 10, 0), new Point(-10, 0, 0))
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60).setkT(0.8)),
+				new Polygon(new Point(-10, 0, -10), new Point(0, -10, -10), new Point(0, -10, 0), new Point(-10, 0, 0))
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60).setkT(0.4)),
+				new Polygon(new Point(10, 0, 0), new Point(0, -10, 0), new Point(-10, 0, 0), new Point(0, 10, 0))
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60).setkT(0.5)),
+				new Triangle(new Point(10, 0, 0), new Point(0, -10, 0), new Point(0, 0, 10)) //
+						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(60).setkT(0.5).setkR(0)), //
+				new Triangle(new Point(10, 0, 0), new Point(0, 10, 0), new Point(0, 0, 10)) //
+						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(60).setkT(1)), //
+				new Triangle(new Point(-10, 0, 0), new Point(0, 10, 0), new Point(0, 0, 10)) //
+						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(60).setkT(0.8)), //
+				new Triangle(new Point(-10, 0, 0), new Point(0, -10, 0), new Point(0, 0, 10)) //
+						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(60).setkT(1)), //
+				new Sphere(new Point(0, 0, 3), 2) //
+						.setEmission(new Color(BLUE)) //
+						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setkT(0.6)));
 
-		scene.lights.add( //
-				new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
-						.setkL(0.0004).setkQ(0.0000006));
-		/*
-		 * // Create the shadowing elements scene.addGeometries( // Shadow plane for
-		 * sphere1 new Plane(new Point(-75, -75, -150), new Point(0, 75, -150), new
-		 * Point(-100, -100, -150)) .setEmission(new Color(0, 0, 0)) .setMaterial(new
-		 * Material().setkR(1)) // Shadow plane for sphere2 new Plane(triangle.getP2(),
-		 * triangle.getP3(), sphere2.getCenter()) .setEmission(new Color(0, 0, 0))
-		 * .setMaterial(new Material().setKr(1)), // Shadow plane for sphere3 new
-		 * Plane(triangle.getP3(), triangle.getP1(), sphere3.getCenter())
-		 * .setEmission(new Color(0, 0, 0)) .setMaterial(new Material().setKr(1)) );
-		 */
+		scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(30, 30, 100), new Vector(0, 0, -1)) //
+				.setkL(4E-5).setkQ(2E-7));
 
-		ImageWriter imageWriter = new ImageWriter("custom2_image_with_shadows", 600, 600);
-		camera.setImageWriter(imageWriter).setRayTracer(new RayTracerBasic(scene)).renderImage().writeToImage();
-	}
-
-	@Test
-	public void effectsTests() {
-		Camera camera = new Camera(new Point(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0));
-		camera.setVPDistance(1000).setVPSize(200, 200);
-		scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), new Double3(0.15)));
-		scene.geometries.add( //
-				new Sphere(new Point(50, 50, 30), 10).setEmission(new Color(java.awt.Color.red))
-						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30).setkT(0).setkR(0.2)),
-				new Sphere(new Point(50, 50, 30), 10).setEmission(new Color(java.awt.Color.BLACK))
-						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(40)),
-				new Sphere(new Point(0, -10, 50), 10).setEmission(new Color(java.awt.Color.CYAN))
-						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)),
-				new Sphere(new Point(50, -10, 30), 30).setEmission(new Color(java.awt.Color.BLUE))
-						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setkT(0.6).setkR(0)),
-				new Sphere(new Point(-20, 50, 45), 10).setEmission(new Color(java.awt.Color.RED))
-						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100)),
-				new Sphere(new Point(-20, 50, 45), 20).setEmission(new Color(java.awt.Color.BLUE))
-						.setMaterial(new Material().setKd(0.4).setKs(0.3).setShininess(100).setkT(0.3)),
-				new Sphere(new Point(0, 15, 30), 10).setEmission(new Color(java.awt.Color.ORANGE))
-						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setkT(0.6).setkR(0)),
-				new Sphere(new Point(-50, -20, 30), 20).setEmission(new Color(java.awt.Color.MAGENTA))
-						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(100).setkT(0).setkR(0.5))
-
-		);
-
-		scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(40, -40, -160), new Vector(-1, 1, 4))
-				.setkC(1).setkL(4E-4).setkQ(2E-5));
-		scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(300, 30, 0), new Vector(-2, 3, 3)).setkC(1)
-				.setkL(4E-4).setkQ(2E-5));
-		camera.setImageWriter(new ImageWriter("ballsInTheAir", 600, 600)).setRayTracer(new RayTracerBasic(scene));
-		camera.renderImage(); //
+		ImageWriter imageWriter = new ImageWriter("refractionShadowPyramid", 600, 600);
+		camera.setImageWriter(imageWriter) //
+				.setRayTracer(new RayTracerBasic(scene)) //
+				.renderImage(); //
 		camera.writeToImage();
-
 	}
-
 }
